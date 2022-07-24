0393b6cb4feeffcdf448176ac9cfc80137e89e0c
Aqil <testuser-aqil@protonmail.com>
2022-07-24 08:31:45 +0400

    add formatter

--- gcd.py
+++ gcd.py
@@ -4,15 +4,17 @@ def gcd(a, b):
 
     elif b == 0:
         return a
-    
+
     elif a == b:
         return a
 
     if a > b:
-        return gcd(a-b, b)
-    return gcd(a, b-a)
+        return gcd(a - b, b)
 
-a = int(input("1st number :"))
-b = int(input("2nd number :"))
+    return gcd(a, b - a)
 
-print(f"GCD of {a} and {b} is {gcd(a, b)}")
+
+a = int(input("1st number : "))
+b = int(input("2nd number : "))
+
+print(f"gcd({a}; {b}) = {gcd(a, b)}")
