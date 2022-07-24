a4ad5d9df9a346e793e0c32b1fb9e5ba384ec791
Aqil <testuser-aqil@protonmail.com>
2022-07-24 14:01:18 +0400

    Update fib.py

--- fib.py
+++ fib.py
@@ -2,8 +2,14 @@ import sys
 
 
 def fib(n):
-    if n <= 2:
+    if n == 0:
         return n
+
+    elif n == 1:
+        return 1
+
+    elif n == 2:
+        return 1
 
     return fib(n - 1) + fib(n - 2)
 
